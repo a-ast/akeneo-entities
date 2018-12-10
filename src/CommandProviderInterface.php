1ebe48b73d77fa3fbc1d760b9e55177d73d94e07
Andrey Astakhov <andrey.astakhov@gmail.com>
2018-12-10 22:57:25 +0100

    Add method of the command provider interface.

--- src/CommandProviderInterface.php
+++ src/CommandProviderInterface.php
@@ -4,5 +4,8 @@ namespace Aa\Akeneo\ImportCommands;
 
 interface CommandProviderInterface
 {
-
+    /**
+     * @return Traversable|\Aa\Akeneo\ImportCommands\CommandInterface[]
+     */
+    public function getCommands(): Traversable;
 }
