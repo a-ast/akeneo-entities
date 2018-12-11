3b0761f54e7817a4696551ff2e550b0f85a89717
Andrey Astakhov <andrey.astakhov@gmail.com>
2018-12-11 21:59:52 +0100

    Add configuration for denormalizer and routing.

--- src/CommandList.php
+++ src/CommandList.php
@@ -4,6 +4,11 @@ namespace Aa\AkeneoImport\ImportCommands;
 
 class CommandList extends \ArrayObject implements CommandListInterface
 {
+    public function __construct(array $items = [])
+    {
+        parent::__construct($items);
+    }
+
     public function add(CommandInterface $command)
     {
         $this->append($command);
