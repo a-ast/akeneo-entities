0fe0b8b906f022cf1939c1eb2cd6bd425acc98a1
Andrey Astakhov <andrey.astakhov@gmail.com>
2019-01-17 21:28:03 +0100

    Remove commandClass from exception.

--- src/ImportCommand/Exception/CommandHandlerException.php
+++ src/ImportCommand/Exception/CommandHandlerException.php
@@ -7,25 +7,5 @@ use Throwable;
 
 class CommandHandlerException extends RuntimeException
 {
-    /**
-     * @var string
-     */
-    private $commandClass;
 
-    /**
-     * @var array
-     */
-    private $errors;
-
-    public function __construct(string $message, string $commandClass, Throwable $previous = null)
-    {
-        parent::__construct($message, 0, $previous);
-
-        $this->commandClass = $commandClass;
-    }
-
-    public function getCommandClass(): string
-    {
-        return $this->commandClass;
-    }
 }
