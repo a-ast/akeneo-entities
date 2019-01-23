d42094e02ac811fe0d3128ba4b698e770f63e580
Andrey Astakhov <andrey.astakhov@gmail.com>
2019-01-23 16:51:20 +0100

    Implement recoverable exception that knows list of failed commands.

--- src/ImportCommand/Exception/RecoverableCommandHandlerException.php
+++ src/ImportCommand/Exception/RecoverableCommandHandlerException.php
@@ -2,9 +2,24 @@
 
 namespace Aa\AkeneoImport\ImportCommand\Exception;
 
-use RuntimeException;
+use Throwable;
 
 class RecoverableCommandHandlerException extends CommandHandlerException
 {
+    /**
+     * @var array
+     */
+    private $commands;
 
+    public function __construct($message = "", $code = 0, array $commands, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->commands = $commands;
+    }
+
+    public function getCommands(): array
+    {
+        return $this->commands;
+    }
 }
