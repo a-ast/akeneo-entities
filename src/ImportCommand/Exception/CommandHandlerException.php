49a52c85e34369c350e127af989f3d1293ddf632
Andrey Astakhov <andrey.astakhov@gmail.com>
2019-01-30 14:24:27 +0100

    Extend command handler exception.

--- src/ImportCommand/Exception/CommandHandlerException.php
+++ src/ImportCommand/Exception/CommandHandlerException.php
@@ -2,9 +2,37 @@
 
 namespace Aa\AkeneoImport\ImportCommand\Exception;
 
+use Aa\AkeneoImport\ImportCommand\CommandInterface;
 use RuntimeException;
+use Throwable;
 
 class CommandHandlerException extends RuntimeException
 {
+    /**
+     * @var CommandInterface
+     */
+    private $command;
 
+    /**
+     * @var array
+     */
+    private $errors;
+
+    public function __construct($message = '', $code = 0, CommandInterface $command, array $errors, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->command = $command;
+        $this->errors = $errors;
+    }
+
+    public function getCommand(): CommandInterface
+    {
+        return $this->command;
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
 }
